36e9aa08ae6abd6b9a215a522b1e8400aef728c8
csbrotherslabs <95954737+csbrotherslabs@users.noreply.github.com>
2026-08-22 14:19:26 -0500

    Route gallery originals to private Spaces

--- apps/galleries/apps.py
+++ apps/galleries/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class GalleriesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.galleries"
+
+    def ready(self):
+        """Route gallery originals to private Spaces when production storage is enabled."""
+        if not settings.USE_SPACES:
+            return
+
+        from .models import GalleryPhoto
+        from .storage_backends import PrivateGalleryStorage
+
+        GalleryPhoto._meta.get_field("file").storage = PrivateGalleryStorage()
